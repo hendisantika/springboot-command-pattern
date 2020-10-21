af74ec4939ef66fa8c638e94ba29d17d722f8562
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-21 19:36:49 +0700

    #3 - Add Spring Boot Command Pattern Entity Module

---
+++ spring-command-pattern-entity/src/main/java/com/hendisantika/springcommandpattern/springcommandpatternentity/SpringCommandPatternEntityApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springcommandpattern.springcommandpatternentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommandPatternEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommandPatternEntityApplication.class, args);
+	}
+
+}
---
+++ spring-command-pattern-entity/src/test/java/com/hendisantika/springcommandpattern/springcommandpatternentity/SpringCommandPatternEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springcommandpattern.springcommandpatternentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommandPatternEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
