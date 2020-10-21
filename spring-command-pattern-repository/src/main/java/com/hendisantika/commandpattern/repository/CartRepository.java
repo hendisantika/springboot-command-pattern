a752ad9c551f5d989fd331ee410139fca082c16a
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-21 20:03:46 +0700

    #12 - Modify pom.xml file & CartRepository in Spring Boot Command Pattern Repositories Module

--- spring-command-pattern-repository/src/main/java/com/hendisantika/commandpattern/repository/CartRepository.java
+++ spring-command-pattern-repository/src/main/java/com/hendisantika/commandpattern/repository/CartRepository.java
@@ -1,5 +1,8 @@
 package com.hendisantika.commandpattern.repository;
 
+import com.hendisantika.commandpattern.entity.Cart;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-command-pattern
@@ -9,5 +12,5 @@ package com.hendisantika.commandpattern.repository;
  * Date: 21/10/20
  * Time: 19.45
  */
-//public interface CartRepository extends ReactiveMongoRepository<Cart, String> {
-//}
+public interface CartRepository extends ReactiveMongoRepository<Cart, String> {
+}
