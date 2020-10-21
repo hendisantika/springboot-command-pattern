abed591912678c6351b6909146dc4febb08f9e35
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-21 19:35:51 +0700

    #2 - Add Spring Boot Command Pattern Repository Module

---
+++ spring-command-pattern-repository/src/main/java/com/hendisantika/springcommandpattern/springcommandpatternrepository/SpringCommandPatternRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springcommandpattern.springcommandpatternrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommandPatternRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCommandPatternRepositoryApplication.class, args);
+    }
+
+}
---
+++ spring-command-pattern-repository/src/test/java/com/hendisantika/springcommandpattern/springcommandpatternrepository/SpringCommandPatternRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springcommandpattern.springcommandpatternrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommandPatternRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
