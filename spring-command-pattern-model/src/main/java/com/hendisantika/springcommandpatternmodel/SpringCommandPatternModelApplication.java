b13b83e2437118107cbb711a261d35b44444d806
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-22 19:18:29 +0700

    #18 - Add Spring Boot Command Pattern Model Module

---
+++ spring-command-pattern-model/src/main/java/com/hendisantika/springcommandpatternmodel/SpringCommandPatternModelApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springcommandpatternmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommandPatternModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommandPatternModelApplication.class, args);
+	}
+
+}
