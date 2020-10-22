2c6ea8c8a425be4561cf58cd2d3067747797bb64
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-22 19:35:43 +0700

    #31 - Add package-info.java in Spring Boot Command Pattern Validation Module

---
+++ spring-command-pattern-validation/src/main/java/com/hendisantika/validation/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-command-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/10/20
+ * Time: 19.35
+ */
+package com.hendisantika.validation;
