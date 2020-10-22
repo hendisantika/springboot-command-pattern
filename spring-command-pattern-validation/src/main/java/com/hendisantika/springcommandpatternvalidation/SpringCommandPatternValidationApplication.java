6fd88e7da2112d5a3bf5a80a9153c780bd9e9c69
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-22 19:19:12 +0700

    #19 - Add Spring Boot Command Pattern Validation Module

---
+++ spring-command-pattern-validation/src/main/java/com/hendisantika/springcommandpatternvalidation/SpringCommandPatternValidationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springcommandpatternvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommandPatternValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommandPatternValidationApplication.class, args);
+	}
+
+}
---
+++ spring-command-pattern-validation/src/test/java/com/hendisantika/springcommandpatternvalidation/SpringCommandPatternValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springcommandpatternvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommandPatternValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
